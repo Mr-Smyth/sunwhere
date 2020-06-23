3da097ebc0da57ebf227cb332b91a503fb692aa9
Eamonn Smyth <e.smyth75@gmail.com>
2020-06-23 21:31:41 +0000

    Added a jasmine test for thisLocation

--- assets/jasmine/spec/sunwhereSpec.js
+++ assets/jasmine/spec/sunwhereSpec.js
@@ -1,23 +1,17 @@
-describe("This is a whatCanIDrink function", function(){
+describe("This is a thisLocation function", function(){
 
-    /* BEFORE EACH BLOCK TEST, A NEW INSTANCE WILL BE CREATED TO TEST, SO NO RESIDUE FROM PREVIOUS TEST */
-    beforeEach(function (){
-        drink = new whatCanIDrink();
-    });
 
-    describe("checks age", function(){
+    describe("check Latitude", function(){
         it("exists", function(){
-            expect(whatCanIDrink).toBeDefined();
+            expect(thisLocation).toBeDefined();
         });
 
-        it("Should return incorrect value if i enter a letter", function(){
-            let result = whatCanIDrink("a")
-            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        it("Should return a float number for latitude", function(){
+            let result = thisLocation();
+            expect(result).toBe(typeof(float));
         });
 
-         it("should return drink beer when called with whatCanIDrink(18)", function () {
-            let result = whatCanIDrink(18)
-            expect(result).toBe("Drink Beer");
-        });
+
     });
 });
+
