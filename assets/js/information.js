5427d7c7d134254c65159b971325f380ca5da104
TheSmyth <e.smyth75@gmail.com>
2020-07-13 01:58:12 +0100

    Add function in information.js called initData
    * InitData gets the required information from local storage theat the
      page needs to display weather and a map

--- assets/js/information.js
+++ assets/js/information.js
@@ -1,2 +1,22 @@
-let selectedLoc = JSON.parse(window.localStorage.getItem('selectedLocation'));
-console.log(selectedLoc);
+function initData() {
+    let latitude = 0;
+    let longitude = 0;
+
+    let selectedLoc = JSON.parse(window.localStorage.getItem('selectedLocation'));
+    console.log(selectedLoc);
+    const weekendWeatherDeserialized = JSON.parse(localStorage.getItem("weekendWeather"));
+    console.log(weekendWeatherDeserialized);
+
+    for (let location in weekendWeatherDeserialized) {
+        if (weekendWeatherDeserialized.hasOwnProperty(location)) {
+            if (selectedLoc === weekendWeatherDeserialized[location].Friday.placeName) {
+                latitude = weekendWeatherDeserialized[location].lat;
+                longitude = weekendWeatherDeserialized[location].lon;
+            }
+        }
+
+    }
+    return { "lat": latitude, "lng": longitude }
+}
+
+console.log(initData());
