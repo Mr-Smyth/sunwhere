b7b845ebe28ab923921691d403f8b46103b7cb85
TheSmyth <e.smyth75@gmail.com>
2020-07-13 19:23:07 +0100

    Add functionality to preload a search for nearby tourist attractions
    After the search is created it will pass the results into createMarkers

--- assets/js/information.js
+++ assets/js/information.js
@@ -31,7 +31,7 @@ function createMap() {
 
     map = new google.maps.Map(document.getElementById("map"), {
         center: coords,
-        zoom: 13,
+        zoom: 15,
         disableDefaultUI: true // REMOVE THE DEFAULT BOXES FROM THE MAP
     });
 
@@ -40,6 +40,21 @@ function createMap() {
     const searchBox = new google.maps.places.SearchBox(input);
 
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+     // DO A NEARBY DEFAULT SEARCH FOR ATTRACTIONS
+     var service = new google.maps.places.PlacesService(map);
+     service.nearbySearch(
+         {
+             location: coords,
+             radius: 5000 ,
+             type: "tourist_attraction"
+         },
+         function (results, status) {
+             if (status !== "OK") return;
+ 
+             createMarkers(results);
+         }
+     );
 }
 
 
