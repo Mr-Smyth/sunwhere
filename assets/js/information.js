1a180fa910f7a6ef86aeb9680e3e59fdfe21c25f
TheSmyth <e.smyth75@gmail.com>
2020-07-13 02:14:48 +0100

    Add the function createMap to information.js
    * Added the function which creates the map.
    * Credit Google places documentation that supplied me with the
      template code for achieving this.
    * Also added the code to push the searchbox onto the rendered map.
    * Added styling to results.css for the map.

--- assets/js/information.js
+++ assets/js/information.js
@@ -19,4 +19,31 @@ function initData() {
     return { "lat": latitude, "lng": longitude }
 }
 
-console.log(initData());
+// CREATE A MAP FOR THE SELECTED LOCATION
+function createMap() {
+
+    let coords = initData();
+
+    /* CREDIT GOOGLE PLACES DOCUMENTATION FOR FOLLOWING CODE
+        WHICH RENDERS THE MAP WITH THE SEARCH BOX AND MARKERS.
+       THE GOOGLE PLACES DOCUMENTATION CODE EXAMPLES HAVE BEEN
+       REFACTORED TO SUIT THE NEEDS OF THIS APPLICATION */
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: coords,
+        zoom: 13,
+        disableDefaultUI: true // REMOVE THE DEFAULT BOXES FROM THE MAP
+    });
+
+    // CREATE THE SEARCH BOX AND POP IT ONTO THE MAP
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+}
+
+
+// DO THIS FIRST
+window.onload = function () {
+    createMap();
+}
